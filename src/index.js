19c7683d0c1e1a6c2a18a48e758f548f42e32882
Christoffer Lambertsson <github@lambertsson.me>
2020-08-10 14:22:57 +0200

    Creates config object with single line settings, does not handle overrides nor multiple settings on a single line

--- src/index.js
+++ src/index.js
@@ -2,6 +2,15 @@ const fs = require("fs");
 
 const isEmptyLine = (string) => string === "";
 const removeComment = (string) => string.split(";")[0];
+const isSingleSettingsValue = (string) => string.indexOf("=") === string.lastIndexOf("=");
+
+const getKeyValuePair = (line) => {
+  const pair = line.split("=");
+  return {
+    key: pair[0].trim(),
+    value: pair[1].trim(),
+  };
+};
 
 const load_config = (path, overrides) => {
   fs.readFile(`${__dirname}/${path}`, function (err, data) {
@@ -32,7 +41,20 @@ const load_config = (path, overrides) => {
       console.log("Could not group lines in config file", error);
     }
 
+    let config = {};
+    groupedLines.forEach((group) => {
+      config[group.name] = {};
+      group.lines.forEach((settingsLine) => {
+        if (isSingleSettingsValue(settingsLine)) {
+          const settingsObject = getKeyValuePair(settingsLine);
+          config[group.name][settingsObject.key] = settingsObject.value;
+        }
+      });
+    });
+
     console.log(groupedLines);
+    console.log("\n");
+    console.log(config);
   });
 };
 
